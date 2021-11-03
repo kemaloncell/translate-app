33e3e9960f96a545fe7a008cc5fe1c7e0cbd0588
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-03 17:24:28 +0300

    created api key and first translate test

--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data() {
     return {
@@ -23,8 +24,14 @@ export default {
   },
   methods: {
     translateIt() {
-      alert(this.translateText);
-      alert(this.translateTo);
+      let url = ' https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20211103T140348Z.9b8263362c116e09.c63c447acbe03118235c2c0fdf3566420e0cd318&text=' + this.translateText + '&lang=en';
+
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((e) => console.log(e));
     },
   },
 };
