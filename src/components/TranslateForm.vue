03a30b1a858c1ea67d5eda4aec0d643222536127
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-11-03 17:38:54 +0300

    supported languages were pulled with api and written into selecbox

--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -2,7 +2,7 @@
   <form @submit.prevent="translateIt" class="well">
     <textarea v-model="translateText" cols="30" rows="5" class="form-control" placeholder="Write the word/sentence you want to translate."></textarea>
     <select v-model="translateTo" class="form-control">
-      <option>1</option>
+      <option v-for="(value, key) in languages" :value="key" :key="key">{{ value }}</option>
     </select>
     <br />
     <div class="text-left">
@@ -20,11 +20,12 @@ export default {
     return {
       translateText: '',
       translateTo: '',
+      languages: {},
     };
   },
   methods: {
     translateIt() {
-      let url = ' https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20211103T140348Z.9b8263362c116e09.c63c447acbe03118235c2c0fdf3566420e0cd318&text=' + this.translateText + '&lang=en';
+      let url = ' https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20211103T140348Z.9b8263362c116e09.c63c447acbe03118235c2c0fdf3566420e0cd318&text=' + this.translateText + '&lang=' + this.translateTo;
 
       axios
         .get(url)
@@ -33,6 +34,14 @@ export default {
         })
         .catch((e) => console.log(e));
     },
+  },
+  created() {
+    axios
+      .get('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20211103T140348Z.9b8263362c116e09.c63c447acbe03118235c2c0fdf3566420e0cd318&ui=en')
+      .then((response) => {
+        this.languages = response.data.langs;
+      })
+      .catch((e) => console.log(e));
   },
 };
 </script>
